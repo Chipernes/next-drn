7a3e2993be92a0d0d30afaa26f0b9501349de110
Nikita Motytskyi <nikonprofiz@gmail.com>
2025-03-23 22:01:56 +0200

    [FIX] - Fixed redirect after verify

--- src/app/(auth)/verify/verify.hook.ts
+++ src/app/(auth)/verify/verify.hook.ts
@@ -1,12 +1,13 @@
 'use client';
 
-import { redirect } from 'next/navigation';
+import { redirect, useRouter } from 'next/navigation';
 import { useSession } from 'next-auth/react';
 import { signInWithCredentials/* , signUp */ } from 'basics/actions/auth';
 import { successToast, warningToast } from 'basics/utils/toast';
 import { AuthFormSubmitCallbackType } from 'components/AuthForm/AuthForm.types';
 
 const useVerify = () => {
+  const router = useRouter();
   const { data: session } = useSession();
 
   const getAllUsers = async () => {
@@ -27,9 +28,12 @@ const useVerify = () => {
     // const result = await signUp(userData);
     const result = await signInWithCredentials(userData);
     if (result.success) {
+      const event = new Event('visibilitychange');
+      document.dispatchEvent(event);
+
       successToast('You have successfully signed in');
 
-      window.location.replace(`/${session?.user?.role.toLowerCase()}`);
+      router.push(`/${session?.user?.role.toLowerCase()}`);
     } else {
       warningToast('Login or password are not valid');
     }
